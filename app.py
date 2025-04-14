3fb3ecd66aec77b8794e62ab0e75cbc72db49674
akaPierre <pierre.ap32@gmail.com>
2025-04-14 13:48:22 -0300

    Add Vercel config and production setup

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
@@ -96,3 +97,6 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True)
+else:
+    with app.app_context():
+        db.create_all()
